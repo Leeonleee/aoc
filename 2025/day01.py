7ba3c0f4ec7bb4e112d52920e6250529a12fbf5d
Leon Lee <leonlee20031219@gmail.com>
2025-12-01 23:37:15 +1100

    added day 1 part 2 solution. Not optimal

--- 2025/day01.py
+++ 2025/day01.py
@@ -1,6 +1,6 @@
 from sys import stdin
 
-def main():
+def part1():
     dial_val = 50
     res = 0
 
@@ -16,5 +16,30 @@ def main():
             res += 1
     print(res)
 
+def part2():
+    dial_val = 50
+    res = 0
+
+    for line in stdin:
+        direction = line[0]
+        rotation = int(line[1:])
+        
+        if direction == "L":
+            for _ in range(rotation):
+                dial_val -= 1
+                if dial_val < 0:
+                    dial_val = 99
+                if dial_val == 0:
+                    res += 1
+        else:
+            for _ in range(rotation):
+                dial_val += 1
+                if dial_val > 99:
+                    dial_val = 0
+                if dial_val == 0:
+                    res += 1
+
+    print(res)
+
 if __name__ == "__main__":
-    main()
+    part2()
